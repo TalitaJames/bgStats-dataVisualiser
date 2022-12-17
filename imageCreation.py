10ee86cf328e22c38d613dd83b69cd4c1637473e
TalitaJames <97495939+TalitaJames@users.noreply.github.com>
2022-12-17 23:54:24 +1100

    Update imageCreation.py
    addded def for top game

--- imageCreation.py
+++ imageCreation.py
@@ -1,32 +1,96 @@
 from PIL import Image, ImageDraw, ImageFont 
+import dataGathering
 
-width = 1080
-height = 1920
 
-#create the coloured overlays
-colorOptions = {
-    'dark_blue':(27,53,81),
-    'grey':(70,86,95),
-    'light_blue':(93,188,210),
-    'blue':(23,114,237),
-    'orange':(242,174,100),
-    'purple':(114,88,136),
-    'red':(255,0,0),
-    'yellow':(255,255,0),
-    'yellow_green':(232,240,165),
-    'green':(65, 162, 77),
-    'white':(255,255,255),
-    'black':(0,0,0)
+def topX(list, x):
+    string=""
+    max=x
+    if len(list)<x:
+        max=len(list)
+        
+    for i in range(max):
+        string += (f"{i+1}. {list[i]}\n")
+
+    return string
+
+
+def gamesWrapped():
+    width = 1080
+    height = 1920
+
+    wrapped=Image.open('pictureAssets/Spotify_Wrapped_Blank.jpg')
+    wrappedDraw = ImageDraw.Draw(wrapped)  
     
-    }
+    font = ImageFont.truetype('pictureAssets/fonts/Courier Prime.ttf', 50)
+    font_heading = ImageFont.truetype('pictureAssets/fonts/Courier Prime.ttf', 40)
+    font_big = ImageFont.truetype('pictureAssets/fonts/Courier Prime Bold.ttf', 60)
+    font_year = ImageFont.truetype('pictureAssets/fonts/Courier Prime Bold.ttf', 112)
 
-imTest=Image.new('RGB', (width,height), color=colorOptions['white']) 
+    wrappedDraw.text((40, 200), 'Games', 'white', font=font_year)
+    
+    wrappedDraw.rectangle([(265,242),(813,790)], fill ="green")
+
+    
+    
+    # 2*height/3=1280
+    players=["Talita James", "Elisa James", "Xander James", "Esther Whitehead", "Joel Donnelly", "Nigel James", "Zack Alloggia", "Aarom Riley"]
+    playerTop5List=topX(players,5)
+    
+    wrappedDraw.text((100,1100),'Most Played',fill='white',font=font_big)
+    wrappedDraw.multiline_text((100, 1175), playerTop5List, 'white', font=font, spacing=20)
+    
+    wrappedDraw.text((570,1100),'Game Played',fill='white',font=font_big)
+    # wrappedDraw.multiline_text(xy, text, fill=None, font=None, anchor=None, spacing=0, align=”left”)
+
+
+    wrapped.save('pictureExports/wrapped.png')
+
+
+def topGames():
+
+    font_heading = ImageFont.truetype('pictureAssets/fonts/CircularStd-Black.ttf', 320)
+    font_game = ImageFont.truetype('pictureAssets/fonts/CircularStd-Medium.ttf', 175)
+    font_subtext = ImageFont.truetype('pictureAssets/fonts/CircularStd-Book.ttf', 150)
+    
+    
+    wrapped=Image.open('pictureAssets/topGamesBlank.jpg')
+    
+    wrappedDraw = ImageDraw.Draw(wrapped)  
+    wrappedDraw.text((500, 820), 'My Top Games', 'black', font=font_heading)
+   
+    
+    wrappedDraw.rectangle([(1900,1800),(4500,6500)], fill ="#F774C4")
+
+    for i in range(5):
+        # game images
+        x_im=940
+        y_im=1600+974*i
+        wrappedDraw.rectangle([(x_im,y_im),(x_im+875,y_im+875)], fill ="green")
+        
+        x_txt=1980
+        y_txt=1820
+        # Game name
+        wrappedDraw.text((x_txt, y_txt+980*i), 'Hard to be the bard', 'black', font=font_game)
+        
+        #subtext  
+        wrappedDraw.text((x_txt, y_txt+980*i+310), 'Christian Borle', 'black', font=font_subtext)
 
 
 
-  
-# create rectangle image
-img1 = ImageDraw.Draw(imTest)  
-img1.rectangle([(width/3,height/3),(2*width/3,2*height/3)], fill ="blue", outline ="red")
+    wrapped.save('pictureExports/topGames.png')
+    
+    
+    # Colour pink = #F774C4
 
-imTest.save('pictureExports/test.png')
+
+
+
+
+
+
+
+if __name__=='__main__':    
+    # data=dataGathering.parseData()
+    # playerData=data['playerData']
+    topGames()
+    print("***** DONE gteen farts *****\n\n")
